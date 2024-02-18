38380f889c9c363317578e271d2ce69c9baaacc7
Ankit Kaushik <ankitkaushik6269@gmail.com>
2024-02-18 21:44:14 +0530

    day 3

--- index.ts
+++ index.ts
@@ -1,2 +1,9 @@
-let myname: number = 12;
-console.log(myname);
+interface computer {
+  (name: string, ram: any);
+}
+
+const ak: computer = (name,ram) => {
+  console.log(`name: ${name}, ram: ${ram}`);
+}
+
+ak("ankit",9);
